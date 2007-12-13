ea17e24b9897d4f23ffa76d3f7d399dc5deca65f
petruss <petruss@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-12-13 09:41:46 +0000

    Version 1.6.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fdfx_beimage/trunk@7491 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ fdfx_be_image/cm1/locallang.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * Language labels for fdfx_be_image module cm1
+ *
+ * This file is detected by the translation tool.
+ */
+
+$LOCAL_LANG = Array (
+	'default' => Array (
+		'tx_fdfxbeimage_title' => 'FXImage',
+		'tx_fdfxbeimage_function1' => 'Crop Image',
+		'tx_fdfxbeimage_function2' => 'Rotate Image',
+		'tx_fdfxbeimage_function3' => 'T.B.D',
+		'tx_fdfxbeimage_crop_text'	=> 'To select crop area, drag and move the dotted rectangle or type in values directly into the form.',
+		'tx_fdfxbeimage_crop_section_header'=>'Image crop',
+		'tx_fdfxbeimage_crop_legend_offset'=>'Crop-Offset',
+		'tx_fdfxbeimage_crop_legend_dimension'=>'Crop-Dimension',
+		'tx_fdfxbeimage_crop_legend_output'=>'Crop-Output',
+		'tx_fdfxbeimage_crop_legend_process'=>'Crop-Process',
+		'tx_fdfxbeimage_crop_label_x'=>'Offset X:',
+		'tx_fdfxbeimage_crop_label_y'=>'Offset Y:',
+		'tx_fdfxbeimage_crop_label_width'=>'Width:',
+		'tx_fdfxbeimage_crop_label_height'=>'Height:',
+		'tx_fdfxbeimage_crop_label_percent'=>'Percent:',
+		'tx_fdfxbeimage_crop_label_convert'=>'Convert to:',
+		'tx_fdfxbeimage_crop_label_fixedsize'=>'Fixed Size:',
+		'tx_fdfxbeimage_crop_label_preview'=>'Preview:',
+		'tx_fdfxbeimage_crop_label_ratio'=>'Keep Ratio:',
+		'tx_fdfxbeimage_crop_label_lock'=>'Lock:',
+		'tx_fdfxbeimage_crop_label_store'=>'Store:',
+		'tx_fdfxbeimage_crop_submit'=>'Crop it!',
+		'tx_fdfxbeimage_crop_setsize'=>'+',
+		'tx_fdfxbeimage_error'=>'ERROR: ',
+		'tx_fdfxbeimage_error_no_image_info'=>'No ImageInfo!',
+		'tx_fdfxbeimage_error_absolute_path_notaccessable'=>'Path %1 should be absolute but can not be accessed by TYPO3',
+		'tx_fdfxbeimage_error_relative_folder_creation'=>'Can not create relative folder %1 in path %2',
+		'tx_fdfxbeimage_success_image_saved'=>'Image saved to %1',
+	),
+	'dk' => Array (
+	),
+	'de' => Array (
+		'tx_fdfxbeimage_title' => 'FXImage',
+		'tx_fdfxbeimage_function1' => 'Bild beschneiden',
+		'tx_fdfxbeimage_function2' => 'Bild drehen',
+		'tx_fdfxbeimage_function3' => 'T.B.D',
+		'tx_fdfxbeimage_crop_text'	=> 'Um Bereich für Zuschnitt auszuwählen, ziehen sie mit den Rechtecken die punktierte Linen an die gewünschte Position oder geben Sie die Werte direkt in die Felder ein.',
+		'tx_fdfxbeimage_crop_section_header'=>'Bild beschneiden',
+		'tx_fdfxbeimage_crop_legend_offset'=>'Offset',
+		'tx_fdfxbeimage_crop_legend_dimension'=>'Maße',
+		'tx_fdfxbeimage_crop_legend_output'=>'Ausgabe',
+		'tx_fdfxbeimage_crop_legend_process'=>'Verarbeitung',
+		'tx_fdfxbeimage_crop_label_x'=>'Offset X:',
+		'tx_fdfxbeimage_crop_label_y'=>'Offset Y:',
+		'tx_fdfxbeimage_crop_label_width'=>'Breite:',
+		'tx_fdfxbeimage_crop_label_height'=>'Höhe:',
+		'tx_fdfxbeimage_crop_label_percent'=>'Faktor in %:',
+		'tx_fdfxbeimage_crop_label_fixedsize'=>'Feste Größe:',
+		'tx_fdfxbeimage_crop_label_convert'=>'Umwandeln in:',
+		'tx_fdfxbeimage_crop_label_preview'=>'Vorschau:',
+		'tx_fdfxbeimage_crop_label_store'=>'Speichern:',
+		'tx_fdfxbeimage_crop_submit'=>'zuschneiden',
+		'tx_fdfxbeimage_error'=>'Fehler: ',
+		'tx_fdfxbeimage_error_no_image_info'=>'Keine Bildinformation verfügbar',
+		'tx_fdfxbeimage_error_absolute_path_notaccessable'=>'Absoluter Pfad %1 kann von TYPO3 nicht ge�ffnet werden!',
+		'tx_fdfxbeimage_error_relative_folder_creation'=>'Kann kein Verzeichnis %1 im Pfad %2 anlegen!',
+		'tx_fdfxbeimage_success_image_saved'=>'Bild gespeichert unter %1',
+	),
+	'no' => Array (
+	),
+	'it' => Array (
+	),
+	'fr' => Array (
+	),
+	'es' => Array (
+	),
+	'nl' => Array (
+	),
+	'cz' => Array (
+	),
+	'pl' => Array (
+	),
+	'si' => Array (
+	),
+	'fi' => Array (
+	),
+	'tr' => Array (
+	),
+	'se' => Array (
+	),
+	'pt' => Array (
+	),
+	'ru' => Array (
+	),
+	'ro' => Array (
+	),
+	'ch' => Array (
+	),
+	'sk' => Array (
+	),
+	'lt' => Array (
+	),
+	'is' => Array (
+	),
+	'hr' => Array (
+	),
+	'hu' => Array (
+	),
+	'gl' => Array (
+	),
+	'th' => Array (
+	),
+	'gr' => Array (
+	),
+	'hk' => Array (
+	),
+	'eu' => Array (
+	),
+	'bg' => Array (
+	),
+	'br' => Array (
+	),
+	'et' => Array (
+	),
+	'ar' => Array (
+	),
+	'he' => Array (
+	),
+	'ua' => Array (
+	),
+	'lv' => Array (
+	),
+	'jp' => Array (
+	),
+	'vn' => Array (
+	),
+	'ca' => Array (
+	),
+	'ba' => Array (
+	),
+	'kr' => Array (
+	),
+	'eo' => Array (
+	),
+	'my' => Array (
+	),
+);
+?>
---
+++ fdfx_be_image/lib/class.fdfx_image_basic.php
@@ -0,0 +1,95 @@
+<?php
+#########################################################################
+#
+# License:
+#    This program is free software; you can redistribute it and/or
+#    modify it under the terms of the MPL Mozilla Public License
+#    as published by the Free Software Foundation; either version 1.1
+#    of the License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    MPL Mozilla Public License for more details.
+#
+#    You may have received a copy of the MPL Mozilla Public License
+#    along with this program.
+#
+#    An on-line copy of the MPL Mozilla Public License can be found
+#    http://www.mozilla.org/MPL/MPL-1.1.html
+#
+# 	Copyright (c) 2006 by 4Many Services
+#   @author: 	Peter Russ <peter.russ@4many.net>
+#   @version:	$Id$
+#
+#   Date:       09.12.2006
+#   Filename:   class.fdfx_image_basic.php
+#
+#   Project:    fdfx_be_image
+#
+##################################################
+class fdfx_Image_Basic
+{
+	var $fileName = '';
+	var $fileNameLocal='';
+	var $conf = array ();
+	var $extKey='';
+	var $returnUrl='';
+	function _init($extKey='fdfx_be_image',$fName='')
+	{
+		$this->extKey=$extKey;
+		if ($fName!='')
+		{
+			$this->setFileName($fName);
+		}
+		$userConf = $GLOBALS['BE_USER']->getTSConfig(strtoupper($extKey));
+		$this->conf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$extKey]);
+		if (isset ($userConf['properties']) && is_array($userConf['properties']))
+		{
+			if (isset ($userConf['properties']['maxWidth']) && $userConf['properties']['maxWidth'] > 0)
+			{
+				$this->conf['MAX_WIDTH'] = $userConf['properties']['maxWidth'];
+			}
+			if (isset ($userConf['properties']['maxHeight']) && $userConf['properties']['maxHeight'] > 0)
+			{
+				$this->conf['MAX_HEIGHT'] = $userConf['properties']['maxHeight'];
+			}
+			if (isset($userConf['properties']['fixedSize']) && $userConf['properties']['fixedSize']!='')
+			{
+				$this->conf['FIXED_SIZE'] = $userConf['properties']['fixedSize'];
+			}
+		}
+		$this->returnUrl=t3lib_div::_GP('returnUrl');
+	}
+	function btn_back($params=array(), $absUrl='')	{
+		global $LANG, $BACK_PATH;
+
+		if ($absUrl) {
+			$url = t3lib_extMgm :: isLoaded('dam')?$absUrl:"javascript:top.goToModule('file_list');";
+		} else {
+			$url = "javascript:top.goToModule('file_list');";
+		}
+
+		$content = '<a href="'.htmlspecialchars($url).'" class="typo3-goBack">'.
+					'<img'.t3lib_iconWorks::skinImg($BACK_PATH,'gfx/goback.gif"', 'width="14" height="14"').' class="absmiddle" alt="" /> '.$LANG->sL('LLL:EXT:lang/locallang_core.xml:labels.goBack',1).
+					'</a>';
+
+		return $content;
+	}
+
+	function setFileName($fName)
+	{
+		if (file_exists($fName))
+		{
+			$this->fileName=$fName;
+		}
+	}
+	function getFileName()
+	{
+		return $this->fileName;
+	}
+}
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/fdfx_be_image/lib/class.fdfx_image_basic.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/fdfx_be_image/lib/class.fdfx_image_basic.php']);
+}
+?>
