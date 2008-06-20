41caf5bb63bb747dea95b047359d83f294deb498
petruss <petruss@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-20 12:59:05 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fdfx_beimage/trunk@9368 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ 1_7_0_alpha/ext_tables.php
@@ -0,0 +1,36 @@
+<?php
+if (!defined('TYPO3_MODE'))
+	die('Access denied.');
+if (TYPO3_MODE == "BE")
+{
+	// add DAM support
+	if (t3lib_extMgm :: isLoaded('dam'))
+	{
+		t3lib_extMgm::insertModuleFunction(
+			'txdamM1_cmd',
+			'tx_fdfxbeimage_modfunc1',
+			t3lib_extMgm::extPath($_EXTKEY).'cm1/class.tx_fdfxbeimage_modfunc1.php',
+			t3lib_extMgm::extPath($_EXTKEY).'cm1/locallang.xml:tx_fdfxbeimage_function1'
+		);
+		t3lib_extMgm::insertModuleFunction(
+			'txdamM1_cmd',
+			'tx_fdfxbeimage_modrotate',
+			t3lib_extMgm::extPath($_EXTKEY).'cm1/class.tx_fdfxbeimage_modrotate.php',
+			t3lib_extMgm::extPath($_EXTKEY).'cm1/locallang.xml:tx_fdfxbeimage_function2'
+		);
+		tx_dam::register_action ('tx_fdfxbeimage_rotateFile',
+			t3lib_extMgm::extPath($_EXTKEY).'class.tx_fdfxbeimage_rotateFile.php:&tx_fdfxbeimage_rotateFile',
+			'after:tx_dam_action_viewFile');
+		tx_dam::register_action ('tx_fdfxbeimage_cropFile',
+			t3lib_extMgm::extPath($_EXTKEY).'class.tx_fdfxbeimage_cropFile.php:&tx_fdfxbeimage_cropFile',
+			'after:tx_dam_action_viewFile');
+	}
+	else
+	{
+		$GLOBALS["TBE_MODULES_EXT"]["xMOD_alt_clickmenu"]["extendCMclasses"][] = array (
+			"name" => "tx_fdfxbeimage_cm1",
+			"path" => t3lib_extMgm :: extPath($_EXTKEY)."cm1/class.tx_fdfxbeimage_cm1.php"
+			);
+	}
+}
+?>
